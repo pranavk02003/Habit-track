c2b83063fb57f97f8cc9ed9c9e2f8af3e1c58433
Pranav <pranavk02003@gmail.com>
2025-12-17 00:35:39 +0530

    initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+<h1 className='text-3xl font-bold '>Habit Tracker</h1>
+<p>No habits added yet.</p>
+
+
+
+    </div>
+  )
+}
+
+export default App
